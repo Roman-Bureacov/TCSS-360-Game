3d3e73f80eed430bd90726d1cc1c75c5ccccd596
RomaBureacov <RomanBureacov@gmail.com>
2025-07-07 17:41:42 -0700

    working library !!!!!!

--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,50 @@
 // Created by iwant on 7/7/2025.
 //
 
+// Example program:
+// Using SDL3 to create an application window
+
 #include <SDL3/SDL.h>
-#include <iostream>
+#include <SDL3/SDL_main.h>
 
 int main(int argc, char* argv[]) {
 
-    std::cout << "Hello World!\n";
-    return 1;
+    SDL_Window *window;                    // Declare a pointer
+    bool done = false;
 
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        std::cerr << "SDL_Init failed: " << SDL_GetError() << '\n';
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL3
+
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "An SDL3 window",                  // window title
+        640,                               // width, in pixels
+        480,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+    );
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("SDL3 Test", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-    if (!window) {
-        std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << '\n';
-        SDL_Quit();
-        return 1;
+    while (!done) {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                done = true;
+            }
+        }
+
+        // Do game logic, present a frame, etc.
     }
 
-    SDL_Delay(3000); // Wait 3 seconds
-
+    // Close and destroy the window
     SDL_DestroyWindow(window);
-    SDL_Quit();
 
+    // Clean up
+    SDL_Quit();
     return 0;
 }
