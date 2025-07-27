6becb6222a7227a47348ca80c9c5c877eca9e597
RomaBureacov <RomanBureacov@gmail.com>
2025-07-27 03:36:26 -0700

    engine work

--- src/eng/eng.cpp
+++ src/eng/eng.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "eng.h"
+
+void eng::processEvents() {
+}
+
+void eng::enqueuePersistentEvent(Event &theEvent) {
+}
+
+void eng::enqueueEvent(const Event& theEvent) {
+    for (auto iter = eventQueue.begin(); iter != eventQueue.end(); iter++) {
+        if (iter->eventOriginCharacter.getID() == theEvent.eventOriginCharacter.getID()) {
+
+        }
+    }
+}
--- src/eng/eng.h
+++ src/eng/eng.h
@@ -5,17 +5,47 @@
 #ifndef ENG_H
 #define ENG_H
 
+#include <functional>
 #include <vector>
 
+#include "Event.h"
+#include "../include/AbstractCharacter.h"
+
+
+/**
+ * Bitz, the engine of the game.
+ *
+ * @author Roman Bureacov
+ * @version July 2025
+ */
 class eng {
 private:
-    std::vector<Character*> entities;
+    std::list<AbstractCharacter&> entities;
+    std::list<AbstractCharacter&> persistentEventQueueEntities;
+    std::list<Event&> eventQueue;
+    std::list<Event&> persistentEventQueue;
 
+    /**
+     * Processes the enqueued events, and adds them to the persistent events as necessary.
+     * Also processes persistent events as well.
+     */
+    void processEvents();
+
+    /**
+     * enqueues persistent events, events that lock characters into a move
+     * @param theEvent the persistent event to enqueue
+     */
+    void enqueuePersistentEvent(Event& theEvent);
 public:
-
-
+    /**
+     * Enqueues an event to occur in the next engine tick.
+     * <br>
+     * Character are limited to enqueue only one event at a time.
+     * Attempting to enqueue another event will overwrite the currently-stored event
+     * by that character.
+     * @param theEvent event to enqueue.
+     */
+    void enqueueEvent(Event& theEvent);
 };
-
-
 
 #endif //ENG_H
