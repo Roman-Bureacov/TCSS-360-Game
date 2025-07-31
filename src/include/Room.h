19e3f5a6d3c5b0af73e5e96ae680b12de1227d02
RileyHop1 <rileyhop1@gamil.com>
2025-07-31 18:40:38 +0000

    Started adding the builder pattern for the room class, this should make things a lot better, will work on it more after double date with gymithy

--- src/include/Room.h
+++ src/include/Room.h
@@ -5,6 +5,7 @@
 #ifndef ROOM_H
 #define ROOM_H
 
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -19,7 +20,19 @@ public:
 
     std::vector<std::vector<std::string>> generateRoom();
     std::vector<AbstractCharacter> getCharacters();
+
     void setCharacters(std::vector<AbstractCharacter> characters);
+
+    Room(const int roomID);
+    ~Room();
+
+    //When a room is being generated it will check these to see if it needs doors in that direction.
+    bool roomNorth;
+    bool roomEast;
+    bool roomWest;
+    bool roomSouth;
+
+    int enemyAmount;
 
 
 
@@ -32,10 +45,31 @@ private:
     const int roomID; //Might not want it to be constant.
 
 
+};
 
+class RoomBuilder {
+
+public:
+    virtual void setRoomNorth(const bool roomNorth);
+    virtual void setRoomEast(const bool roomEast);
+    virtual void setRoomWest(const bool roomWest);
+    virtual void setRoomSouth(const bool roomSouth);
+
+    virtual std::shared_ptr<Room> build();
+
+};
+class ConcreteRoomBuilder : public RoomBuilder {
+public:
+    ConcreteRoomBuilder();
+    void setRoomNorth(const bool roomNorth) override;
+    void setRoomEast(const bool roomEast) override;
+    void setRoomWest(const bool roomWest) override;
+    void setRoomSouth(const bool roomSouth) override;
+    std::shared_ptr<Room> build() override;
 
 
 };
+
 
 
 
