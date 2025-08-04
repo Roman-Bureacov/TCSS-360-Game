ab411648999a0160b46b03234444c547a3f7d711
RomaBureacov <RomanBureacov@gmail.com>
2025-08-03 17:02:48 -0700

    removed ID generation being unsigned (not necessary)

--- src/app/model/Characters/AbstractCharacter.cpp
+++ src/app/model/Characters/AbstractCharacter.cpp
@@ -25,7 +25,7 @@ const std::string& AbstractCharacter::getName() const {
     return myName;
 }
 
-unsigned long long AbstractCharacter::getID() const {
+long long AbstractCharacter::getID() const {
     return myID;
 }
 
