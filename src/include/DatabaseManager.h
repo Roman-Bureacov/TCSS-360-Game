e54bf3169bdcf86b2f5352ed16c27a0a7af8fbeb
RomaBureacov <RomanBureacov@gmail.com>
2025-08-21 11:39:52 -0700

    set up database instances, one for static the other for dynamic

--- src/include/DatabaseManager.h
+++ src/include/DatabaseManager.h
@@ -78,8 +78,10 @@ public:
 
 
 private:
-    static constexpr const char* DATABASE_PATH = "DungeonDatabase.db";
-    static sqlite3* DATABASE;
+    static constexpr const char* BUILD_PATH = "DungeonBuildDatabase.db";
+    static constexpr const char* INSTANCE_PATH = "DungeonInstanceDatabase.db";
+    static sqlite3* BUILD_DATABASE;
+    static sqlite3* INSTANCE_DATABASE;
     static const DatabaseManager INSTANCE;
 
     /**
@@ -117,6 +119,13 @@ private:
      * Saves the type table.
      */
     static void saveTypeTable();
+
+    /**
+     * Runs an SQL query. Intended for DDL statements.
+     * @param database the database to execute against
+     * @param sql the sql statement to execute
+     */
+    static void execute(sqlite3 *database, const char *sql);
 };
 
 #endif //DATABASEMANAGER_H
